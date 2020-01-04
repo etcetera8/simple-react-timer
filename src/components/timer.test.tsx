5d75d625a6f0482bbccbe293dda4331b84683ddc
parker lindley <parker.t.lindley@gmail.com>
2020-01-03 21:33:19 -0700

    Wireframe up tests

---
+++ src/components/timer.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Timer from './timer';
+
+import { render, fireEvent, waitForElement } from '@testing-library/react';
+
+describe("<Timer />", () => {
+    test("should display a timer", async() => {
+        
+    })
+})
